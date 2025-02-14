746f236d8844cd530e493ed9fe21fe5166825f50
MilanKoshy24 <milanmkoshy@gmail.com>
2025-02-14 10:56:35 +0530

    last commit

--- AutomationCourse/src/test/java/seleniumBasics/Base.java
+++ AutomationCourse/src/test/java/seleniumBasics/Base.java
@@ -14,15 +14,32 @@ public class Base {
 		driver = new ChromeDriver();
 		//driver = new BraveDriver();
 		
-		driver.get(" https://selenium.qabible.in/");
+		//driver.get("https://selenium.qabible.in/");
+		driver.get("https://www.amazon.in/");
 		driver.manage().window().maximize();
 		
 	}
+	
+	
+	public void closeandQuit() {
+		
+		
+		//driver.close(); //to close single page in a browser
+		
+		driver.quit(); //to close the whole browser
+		
+		
+		
+	}
+	
+	
+	
 
 	public static void main(String[] args) {
 		
 		Base base = new Base();
 		base.initializeBrowser();
+		
 
 	}
 
---
+++ AutomationCourse/src/test/java/seleniumBasics/BrowserCommands.java
@@ -0,0 +1,58 @@
+package seleniumBasics;
+
+
+public class BrowserCommands extends Base { //extends base class 
+	
+	
+	
+	public void browserCommands() {
+		
+		String url = driver.getCurrentUrl(); //to get url of the page
+		String title = driver.getTitle(); // to get site title
+		String handleID = driver.getWindowHandle(); //to get handle id
+		String pageSource = driver.getPageSource(); //to get page source
+		
+		
+		
+		System.out.println(title); //title printing
+		System.out.println(url); //url printing
+		System.out.println(handleID); //handle id printing
+		System.out.println(pageSource); //page source printing
+		
+	}
+	
+	public void navigationCommands() {
+		
+		
+		driver.navigate().to("https://www.flipkart.com"); //to get into another site
+		driver.navigate().back(); //to get back to  main site
+		driver.navigate().forward(); // to get back to the visited site
+		driver.navigate().refresh(); // to refresh the page
+	}
+	
+	
+
+	public static void main(String[] args) {
+		
+		
+	BrowserCommands browsercommands = new BrowserCommands(); //creating object
+	
+	browsercommands.initializeBrowser(); //invoking base.java class
+	//browsercommands.browserCommands(); //invoking current class method
+	
+	
+	browsercommands.navigationCommands(); //invoking Navigation commands
+	
+	
+	
+	
+	
+	
+	
+	
+	//browsercommands.closeandQuit(); // invoking closing class
+		
+		
+	}
+
+}
